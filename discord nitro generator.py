cf297e0e3532dde09151aa4f221d79eef3944a9c
Sidon_OO0 <70560640+SidonTheTroll@users.noreply.github.com>
2022-07-24 18:19:19 +0530

    Update discord nitro generator.py

--- discord nitro generator.py
+++ discord nitro generator.py
@@ -4,6 +4,10 @@ import string
 # it prints links for getting nitro for free. Wydm?
 letters = string.ascii_letters
 numbers = string.digits
-link = print('https://discord.gift/')
-code = print( ''.join(random.choice(letters+numbers) for i in range(20)) )
 
+for i in range (60):
+    base = ('https://discord.gift/') 
+    array = ( ''.join(random.choice(letters+numbers) for i in range(20)) )
+
+for i in range (1): #this function is unused because i dont know how it works :hyperLaugh:
+    print(base + array)
